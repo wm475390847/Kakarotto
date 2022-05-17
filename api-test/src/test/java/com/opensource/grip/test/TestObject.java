daa85e4dc67442be98fd8d0ff809e2bd9ea71d09
王敏 <wangmin.wm@shuwen.com>
2022-05-17 13:11:11 +0800

    删除测试文件

--- api-test/src/test/java/com/opensource/grip/test/TestObject.java
+++ api-test/src/test/java/com/opensource/grip/test/TestObject.java
@@ -1,35 +1,8 @@
 package com.opensource.grip.test;
 
-import com.opensource.grip.generate.core.ApiGenerator;
-import com.opensource.grip.generate.core.IGenerator;
-import com.opensource.grip.generate.parse.IParse;
-import com.opensource.grip.generate.parse.YApiParse;
-import com.opensource.grip.generate.pojo.ApiInfo;
-import org.testng.annotations.Test;
-
 /**
  * @author wangmin
  * @date 2022/5/17 11:28
  */
 public class TestObject {
-
-    @Test
-    public void testCreateApi() {
-        String token =
-                "dingtalk_sso_jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7InVzZXJpZCI6IjE2MzE1ODMzNDg0ODAzMDE2IiwibmljayI6IuiKveiPnCIsInVuaW9uaWQiOiJYOWNBNVl0WUQ4MGtnaWk1aVM2MW55SkFpRWlFIiwiYXZhdGFyIjoiaHR0cHM6Ly9zdGF0aWMtbGVnYWN5LmRpbmd0YWxrLmNvbS9tZWRpYS9sQUxQRGVDMjVOdXM5cmJOQS1qTkEtZ18xMDAwXzEwMDAucG5nIn0sImlhdCI6MTY1MjM0NTc2NSwiZXhwIjoxNjUyOTUwNTY1fQ.F0_TuFdi44AA--zMZrglTsJLmGA60HwwblBuTmC8vV4; _yapi_uid=1415; _yapi_token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1aWQiOjE0MTUsImlhdCI6MTY1Mjc1NzY5NiwiZXhwIjoxNjUzMzYyNDk2fQ.QeflsStat5MHMTbIV6PYXIfUzKSzYHN8daadmmN7luA";
-        IParse<ApiInfo> parse = new YApiParse.Builder()
-                .host("http://yapi.xinhuazhiyun.com")
-                .token(token)
-                .key(1093)
-                .build();
-
-        IGenerator generator = new ApiGenerator.Builder()
-                .parse(parse)
-                .outputPath("src/test/java/com/opensource/grip/test/auto")
-                .templatePath("src/main/resources/template/")
-                .templateName("apiTemplate.ftl")
-                .build().load();
-        generator.execute();
-
-    }
 }
